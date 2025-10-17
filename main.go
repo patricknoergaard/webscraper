3252301eda9169d2a60fead0e50bddd1cfb03271
Patrick Kristensen <patricknoergaard100@gmail.com>
2025-10-17 22:46:53 +0200

    Go function test

--- main.go
+++ main.go
@@ -3,38 +3,15 @@ package main
 import (
 	"fmt"
 	// "os"
-
-	"github.com/gocolly/colly"
 )
 
-func main() {
-
-	c := colly.NewCollector(
-		colly.AllowedDomains("finance.yahoo.com"),
-		colly.MaxDepth(1),
-	)
-
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-
-		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-
-		c.Visit(e.Request.AbsoluteURL(link))
-	})
-
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
-	})
-
-	c.OnError(func(r *colly.Response, err error) {
-		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
-	})
-
-	// os.Args()
-	c.Visit("https://finance.yahoo.com/quote/TSLA/")
+func add(num1 int, num2 int) int {
+	return num1 + num2
 
 }
 
-func select_ticker(ticker string) {
+func main() {
+	var answer int = add(1, 2)
 
+	fmt.Println(answer)
 }
